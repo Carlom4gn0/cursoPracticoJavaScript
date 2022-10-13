f7c97b611616a7d24c6b8b3c5c7ef7c46be84376
Jose Carlomagno <jos3.74502@gmail.com>
2022-10-13 07:28:39 -0300

    aprendiendo a modificar el DOM innerHTML, innerText

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ console.log({
     parra,
     input,
 });
+
+//(selector).innerHTML modifica lo que el selector muestra, remplaza el atributo de la variable(se puede insertar tanto texto como codigo html)
+//(selector).innerText modifica el campo de texto osea q solo va a mostrar lo q se escriba y no ejecutar html
+h1.innerHTML = "buenos dias"
+h1.innerText = "buenos dias"
+//getAttribute(´')lee el atributo de una clase o elemento
+//console.log(h1.getAttribute('class'));
+
+//setAttribute('valor actual', 'valor nuevo') modifica el atributo
+//h1.setAttribute('class','ROjo');
+
+h1.classList.add('hola');//agrega una clase
+h1.classList.remove('nada');
