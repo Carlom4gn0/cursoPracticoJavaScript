65dd7150c8b919c6be1e1bd92c696ccc9b022584
Jose Carlomagno <jos3.74502@gmail.com>
2022-10-13 15:32:27 -0300

    eventos en js

--- script.js
+++ script.js
@@ -16,10 +16,12 @@
 const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
-const btn = document.querySelector('btnCalcular')
+const btn = document.querySelector('btnCalcular');
+const p =document.querySelector('#result');
 
 function btnOnClick(){
-    
+    p.innerText = input1.value +input2.value;
+
 }
 
 
