efd2430a9149d153ec4d13dd92d9a92584078e28
Jose Carlomagno <jos3.74502@gmail.com>
2022-10-13 15:19:42 -0300

    eventos en js

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 //Es importante crear una variable para cada selector de HTML
 // como en CSS es importante con queryselector ingreser ("tipo de selector como en css")
-
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafo = document.getElementsByClassName('.parrafo');
-const parra = document.getElementById('#parra');
-const input = document.querySelector('input');
-
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parrafo,
-    parra,
-    input,
-});
-
 //(selector).innerHTML modifica lo que el selector muestra, remplaza el atributo de la variable(se puede insertar tanto texto como codigo html)
 //(selector).innerText modifica el campo de texto osea q solo va a mostrar lo q se escriba y no ejecutar html
-h1.innerHTML = "buenos dias"
-h1.innerText = "buenos dias"
+//h1.innerHTML = "buenos dias"
+//h1.innerText = "buenos dias"
 //getAttribute(´')lee el atributo de una clase o elemento
 //console.log(h1.getAttribute('class'));
 
 //setAttribute('valor actual', 'valor nuevo') modifica el atributo
 //h1.setAttribute('class','ROjo');
 
-h1.classList.add('hola');//agrega una clase
-h1.classList.remove('nada');
+//h1.classList.add('hola');//agrega una clase
+//h1.classList.remove('nada');
+
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('btnCalcular')
+
+function btnOnClick(){
+    
+}
+
+
+
